cc625771798d06b0d481c6d9235f46bc89616518
artangente00 <98647236+artangente00@users.noreply.github.com>
2022-03-21 03:01:26 +0800

    Add files via upload

---
+++ final_project/machinetranslation/tests.py
@@ -0,0 +1,14 @@
+import unittest
+
+from translator import englishToFrench, frenchToEnglish
+from translator import englishText, frenchText
+
+class Tests(unittest.TestCase):
+    def test_englishToFrench(self):
+        self.assertEqual(englishToFrench(englishText),'This is python programming')
+
+    def test_frenchToEnglish(self):
+        self.assertEqual(frenchToEnglish(frenchText),'Il s agit d une programmation Python')
+
+if __name__=='__main__':
+    unittest.main()
